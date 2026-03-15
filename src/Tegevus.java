b90c2adfe70b35069fd4aa5f47a24e916a37609b
Nansen Palo <nansen.palo@gmail.com>
2026-03-15 15:53:48 +0200

    startup fix

--- src/Tegevus.java
+++ src/Tegevus.java
@@ -3,25 +3,25 @@ import java.util.Random;
 
 public class Tegevus {
 
-    public void töötajaPalkamine(String nimi){
-        int kordaja = StartUp.getTöötajad().length;
+    public void töötajaPalkamine(StartUp startup, String nimi){
+        int kordaja = startup.getTöötajad().size();
         Töötaja uusTöötaja = new Töötaja(nimi, kordaja);
     }
 
-    public void turundusKampaania(){
-        double koef = getKlientideArv() * (1 + Math.random());
+    public void turundusKampaania(StartUp startup){
+        double koef = startup.getKlientideArv() * (1 + Math.random());
     }
 
-    public void töökuseReroll(){
-        List<Töötaja> töötajad = getTöötajad();
+    public void töökuseReroll(StartUp startup){
+        List<Töötaja> töötajad = startup.getTöötajad();
         Random rand = new Random();
         Töötaja töötaja = töötajad.get(rand.nextInt(töötajad.size()));
         Random rand1 = new Random();
         töötaja.setTöökus(rand1.nextDouble() * 0.99);
     }
 
-    public void töökuseReroll(String nimi){
-        List<Töötaja> töötajad = getTöötajad();
+    public void töökuseReroll(StartUp startup, String nimi){
+        List<Töötaja> töötajad = startup.getTöötajad();
         for (Töötaja t : töötajad){
             if (t.getNimi().equals(nimi)){
                 Random rand = new Random();
