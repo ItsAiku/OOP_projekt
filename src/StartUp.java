b4891a5094aa5fc0d49f0c51c52eac86239f2e52
ItsAiku <allikaivar5@gmail.com>
2026-03-15 14:31:35 +0200

    test

---
+++ src/StartUp.java
@@ -0,0 +1,5 @@
+public class StartUp {
+    private int kapital;
+    private list<Töötajad>;
+    private int klientideArv;
+}
---
+++ src/Töötajad.java
@@ -0,0 +1,5 @@
+public class Töötajad {
+    private String nimi;
+    private int palk;
+    private int töökus;
+}
