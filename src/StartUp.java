e8ea7502237938e2674a7b90589c25cd57797314
Nansen Palo <nansen.palo@gmail.com>
2026-03-15 14:43:53 +0200

    .

--- src/StartUp.java
+++ src/StartUp.java
@@ -1,5 +1,32 @@
+import java.util.List;
+
 public class StartUp {
     private int kapital;
-    private list<Töötajad> töötajad;
+    private List<Töötaja> töötajad;
     private int klientideArv;
+
+    public StartUp(int kapital, List<Töötaja> töötajad, int klitideArv) {
+        this.kapital = kapital;
+        this.töötajad = töötajad;
+        this.klientideArv = klitideArv;
+    }
+
+    public void setKapital(int uusKapital) {
+        this.kapital = uusKapital;
+    }
+    public void suurendaKapital(int lisaKapital) {
+        this.kapital += lisaKapital;
+    }
+    public int getKapital() {
+        return kapital;
+    }
+    public void suurendaKliente(int uuedKliendid) {
+        this.klientideArv += uuedKliendid;
+    }
+    public int getKlitideArv() {
+        return klientideArv;
+    }
+    public void lisaTöötaja(Töötaja uusTöötaja) {
+        töötajad.add(uusTöötaja);
+    }
 }
