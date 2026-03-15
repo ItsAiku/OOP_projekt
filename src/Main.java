db6bdfc407a7b1a60b4f826b9b55a494dda1e64d
Nansen Palo <nansen.palo@gmail.com>
2026-03-15 16:13:34 +0200

    maint -> test

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,4 @@
 public class Main {
     public static void main(String[] args) {
-        StartUp ahv = new StartUp();
-        Tegevus tegevus = new Tegevus();
-        tegevus.perkid(7,ahv);
-        System.out.println(ahv.getKlientideArv());
     }
 }
--- src/test.java
+++ src/test.java
@@ -1,6 +1,8 @@
-public class test {
-
-    static void main() {
-
+public class Test {
+    public static void main(String[] args) {
+        StartUp ahv = new StartUp();
+        Tegevus tegevus = new Tegevus();
+        tegevus.perkid(7,ahv);
+        System.out.println(ahv.getKlientideArv());
     }
 }
