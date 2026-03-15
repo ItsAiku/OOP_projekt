f2ea5934ac25cb0e93ad82246d48a77e4e57d3f6
Nansen Palo <nansen.palo@gmail.com>
2026-03-15 16:13:42 +0200

    maint -> test

--- src/test.java
+++
@@ -1,8 +0,0 @@
-public class Test {
-    public static void main(String[] args) {
-        StartUp ahv = new StartUp();
-        Tegevus tegevus = new Tegevus();
-        tegevus.perkid(7,ahv);
-        System.out.println(ahv.getKlientideArv());
-    }
-}
